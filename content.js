d680f6b5b18b1b7c479336ad992eb91b6ee2222f
matthew.sherrill <matthew.sherrill@asynchrony.com>
2021-10-15 11:38:12 -0600

    Add copy to clipboard button - MS

--- content.js
+++ content.js
@@ -1,12 +1,36 @@
 console.info("Adding button to copy wishlist");
 
-async function copyToTextarea() {
-    console.log("Entering", this)
+function copyToTextarea() {
+    console.log("Entering copyToTextArea");
     const wishlistText = document.querySelector("div > form > textarea");
+    const errorSpan = document.getElementById("wishlistErrors")
     const text = wishlistText.value;
-
     const textarea = document.getElementById("wishlistTextarea");
-    textarea.value += text + "\n";
+    if (isInWishlist(text, textarea.value)) {
+        errorSpan.style.display = "block";
+        errorSpan.innerText = "This item already exists in wishlist."
+    }
+    else {
+        textarea.value += text + "\n";
+        errorSpan.style.display = "none";
+        errorSpan.innerText = "";
+    }
+}
+
+async function copyWishlistToClipboard() {
+    const wishlistText = document.getElementById("wishlistTextarea").value;
+
+    await navigator.clipboard.writeText(wishlistText);
+}
+
+function isInWishlist(text, existingText) {
+    const items = existingText.split("\n").filter(i => i);
+    for (let i = 0; i < items.length; i++) {
+        if (text === items[i]) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function toggleWishlist() {
@@ -29,15 +53,23 @@ function contains(selector, text) {
     });
 }
 
-function addElements() {
-    const root = document.getElementById("root");
-    const main = document.getElementById("main");
-    
-    const div = document.createElement("div");
-    div.style.display = "flex";
-    div.style.flexDirection = "column";
-    div.id = "wishlistDiv";
+function getAddToWishlistButton() {
+    const addButton = document.createElement("div");
+    addButton.innerText = "Add Current Item to Wishlist";
+    addButton.id = "addButton";
+    addButton.style.height = "20px";
+    addButton.style.boxShadow = "rgb(245, 245, 245) 0px 0px 0px 1px inset";
+    addButton.style.marginTop = "10px";
+    addButton.style.marginBottom = "10px";
+    addButton.style.padding = "5px";
+    addButton.style.color = "rgb(255, 255, 255)";
+    addButton.style.cursor = "pointer";
+    addButton.addEventListener("click", copyToTextarea, false);
 
+    return addButton;
+}
+
+function getToggleButton() {
     const toggleButton = document.createElement("div");
     toggleButton.id = "toggleButton";
     toggleButton.innerText = "Hide Wishlist";
@@ -46,27 +78,68 @@ function addElements() {
     toggleButton.style.boxShadow = "rgb(245, 245, 245) 0px 0px 0px 1px inset";
     toggleButton.style.padding = "5px";
     toggleButton.addEventListener("click", toggleWishlist, false);
-    root.insertBefore(toggleButton, main);
 
-    const addButton = document.createElement("button");
-    addButton.innerText = "Add Current Item to Wishlist";
-    addButton.id = "addButton";
-    addButton.style.height = "20px";
+    return toggleButton;
+}
 
+function getWishlistTextArea() {
     const textarea = document.createElement("textarea");
     textarea.cols = 100;
     textarea.rows = 50;
     textarea.id = "wishlistTextarea";
+    textarea.style.color = "rgb(255, 255, 255)";
+    textarea.style.display = "inline-block";
+    textarea.style.boxShadow = "rgb(245, 245, 245) 0px 0px 0px 1px inset";
+    textarea.style.background = "rgba(255, 255, 255, 0.05)";
 
-    root.appendChild(div);
+    return textarea;
+}
 
-    console.log("Adding button");
-    div.appendChild(addButton);
+function getCopyToClipboardButton() {
+    const copyButton = document.createElement("div");
+    copyButton.innerText = "Copy to Clipboard";
+    copyButton.id = "copyToClipboardButton";
+    copyButton.style.height = "20px";
+    copyButton.style.boxShadow = "rgb(245, 245, 245) 0px 0px 0px 1px inset";
+    copyButton.style.marginBottom = "10px";
+    copyButton.style.padding = "5px";
+    copyButton.style.color = "rgb(255, 255, 255)";
+    copyButton.style.cursor = "pointer";
+    copyButton.addEventListener("click", copyWishlistToClipboard, false);
 
-    console.log("Adding textarea");
-    div.appendChild(textarea);
+    return copyButton;
+}
 
-    document.getElementById("addButton").addEventListener("click", copyToTextarea, false);
+function getErrorSpan() {
+    const errorSpan = document.createElement("span");
+    errorSpan.id = "wishlistErrors";
+    errorSpan.style.color = "rgb(255, 255, 255)";
+    errorSpan.style.background = "rgb(255, 0, 0, 0.25)";
+    errorSpan.style.padding = "2px";
+    errorSpan.style.letterSpacing = "0.2em";
+    errorSpan.style.display = "none";
+
+    return errorSpan;
+}
+
+function addElements() {
+    const root = document.getElementById("root");
+    const main = document.getElementById("main");
+
+    const wishlistDiv = document.createElement("div");
+    wishlistDiv.style.display = "flex";
+    wishlistDiv.style.flexDirection = "column";
+    wishlistDiv.id = "wishlistDiv";
+
+    const toggleButton = getToggleButton();
+
+    root.appendChild(wishlistDiv);
+
+    wishlistDiv.appendChild(getAddToWishlistButton());
+    wishlistDiv.appendChild(getCopyToClipboardButton());
+    wishlistDiv.appendChild(getWishlistTextArea());
+    wishlistDiv.appendChild(getErrorSpan());
+
     document.addEventListener("keydown", (event) => {
         if (event.key === "Insert") {
             copyToTextarea();
@@ -75,7 +148,6 @@ function addElements() {
 
     const span = contains("span", "Gunsmith")[0];
     span.parentElement.insertBefore(toggleButton, span);
-
 }
 
 let observer = new MutationObserver((mutations) => {
